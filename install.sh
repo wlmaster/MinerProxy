b11adadae531513a14a16da815d79bca082f2d58
wlmaster <96415653+wlmaster@users.noreply.github.com>
2022-01-09 00:46:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ install_download() {
         service supervisord restart
     fi
     [ -d ./MinerProxy ] && rm -rf ./MinerProxy
-    git clone https://github.com/wlmaster/Minerproxy.git
+    git clone https://github.com/wlmaster/MinerProxy.git
 
     if [[ ! -d ./MinerProxy ]]; then
         echo
